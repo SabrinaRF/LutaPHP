1a7c3b8dbc0edd37e09c04819b359aad61d73a5b
sabrina <sabrinarfernandes.srf@gmail.com>
2023-03-03 11:36:08 -0300

    classe luta

--- Luta.php
+++ Luta.php
@@ -4,6 +4,67 @@
  *
  * @author sabri
  */
+require_once 'Lutador.php';
 class Luta {
-    //put your code here
+    private $desafiado;
+    private $desafiante;
+    private $rounds;
+    private $aprovada;
+
+    public function getDesafiado() {return $this->desafiado;}
+    public function getDesafiante() {return $this->desafiante;}
+    public function getRounds() {return $this->rounds;}
+    public function getAprovada() {return $this->aprovada;}
+    
+    public function setDesafiado($desafiado): void {
+        $this->desafiado = $desafiado;
+    }
+    public function setDesafiante($desafiante): void {
+        $this->desafiante = $desafiante;
+    }
+    public function setRounds($rounds): void {
+        $this->rounds = $rounds;
+    }
+    public function setAprovada($aprovada): void {
+        $this->aprovada = $aprovada;
+    }
+    
+    public function marcarLuta($l1, $l2) {
+        if($l1->getCategoria() === $l2->getCategoria() && ($l1 !=$l2)){
+            $this->aprovada=true;
+            $this->desafiado=$l1;
+            $this->desafiante=$l2;
+        }else{
+            $this->aprovada=false;
+            $this->desafiado=null;
+            $this->desafiante=null;
+        }
+    }
+    public function lutar() {
+        if($this->aprovada){
+            $this->desafiado->apresentar();
+            $this->desafiante->apresentar();
+            $vencedor = rand(0,2);
+            switch ($vencedor){
+                case 0:
+                    echo "<p>Empate!</p>";
+                    $this->desafiado->empatarLuta();
+                    $this->desafiante->empatarLuta();
+                    break;
+                case 1:
+                    echo "<p>Ganhador!</p>". $this->desafiado->getNome();
+                    $this->desafiado->ganharLuta();
+                    $this->desafiante->perderLuta();
+                    break;
+                case 2:
+                    echo "<p>Ganhador!</p>". $this->desafiante->getNome();
+                    $this->desafiado->perderLuta();
+                    $this->desafiante->ganharLuta();
+                    break;
+            }
+        }else{
+            echo "<p>Luta não pode acontecer</p>";
+        }
+    }
+
 }
--- index.php
+++ index.php
@@ -11,12 +11,14 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     <body>
         <?php
             require_once 'Lutador.php';
+            require_once 'Luta.php';
             $l=array();
             $l[0] = new Lutador("PREETY", "França", 30, 1.75, 68.9, 11, 2, 1);
             $l[1] = new Lutador("PUTSCRIPT", "Brasil", 29, 1.68, 57.8, 14, 2, 0);
-            
-            $l[0]->status();
-            $l[0]->apresentar();
+                   
+            $uec = new Luta();
+            $uec->marcarLuta($l[0], $l[1]);
+            $uec->lutar();
         ?>
     </body>
 </html>
